7ba597f7be7f84fa7d244c3e6fc6d9c5ea692d71
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-19 19:34:24 -0400

    songs by artist

--- songs.js
+++ songs.js
@@ -14,12 +14,53 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log(`connected as ${connection.threadId}`);
 
+    // testConnection();
+    songsByArtist("eminem");
+});
+
+function displayResults(res) {
+    for (let i = 0; i < res.length; i++) {
+        console.log(`${res[i].song} by ${res[i].artist}`);
+    }
+}
+
+function testConnection() {
     // test query for songs from after 2005
-    connection.query("SELECT * FROM top5000 WHERE year > 2005", function (err, res) {
+    connection.query("SELECT * FROM top5000 WHERE year > 2010", function (err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
         console.log(`Rows: ${res.length}`);
+        displayResults(res);
         connection.end();
     });
 
-});
+}
+
+function songsByArtist(artist) {
+    connection.query("SELECT * FROM top5000 WHERE ?", { artist: artist }, function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(`${res.length} songs from ${artist}:`);
+        displayResults(res);
+        connection.end();
+    });
+
+}
+
+function artistsMoreThanOnce() {
+
+}
+
+function dataRange() {
+
+}
+
+function dataForSong() {
+
+}
+/*
+  * A query which returns all data for songs sung by a specific artist
+  * A query which returns all artists who appear within the top 5000 more than once
+  * A query which returns all data contained within a specific range
+  * A query which searches for a specific song in the top 5000 and returns the data for it
+*/
