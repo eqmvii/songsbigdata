99daa895a50e439cf804b222529fe6aafac40770
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-19 19:27:33 -0400

    test query

--- songs.js
+++ songs.js
@@ -3,15 +3,23 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password", // so secure I know
-  database: "top_songsDB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password", // so secure I know
+    database: "top_songsDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log(`connected as ${connection.threadId}`);
-    connection.end();
-  });
+
+    // test query for songs from after 2005
+    connection.query("SELECT * FROM top5000 WHERE year > 2005", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(`Rows: ${res.length}`);
+        connection.end();
+    });
+
+});
